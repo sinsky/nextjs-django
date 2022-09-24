6d0352f03ea00e60589bfc9be9003139cc5dc8e9
sinsky <60662802+sinsky@users.noreply.github.com>
2022-09-25 03:53:41 +0900

    os library installed

--- drf-jwt/build.vercel.sh
+++ drf-jwt/build.vercel.sh
@@ -1,3 +1,5 @@
+echo "### apt"
+apt-get update && apt-get install libsqlite3-dev libbz2-dev libncurses5-dev libgdbm-dev liblzma-dev libssl-dev tcl-dev tk-dev libreadline-dev
 echo "### requirements install"
 python3 -m pip install -r requirements.dev.txt
 
