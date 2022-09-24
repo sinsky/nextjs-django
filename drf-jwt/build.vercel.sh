03c353f3470faea3e156fa30b44d097315b2bfdc
sinsky <60662802+sinsky@users.noreply.github.com>
2022-09-25 03:42:51 +0900

    add debug command

--- drf-jwt/build.vercel.sh
+++ drf-jwt/build.vercel.sh
@@ -1,11 +1,14 @@
-echo "requirements install"
+echo "### requirements install"
 python3 -m pip install -r requirements.txt
 
-echo "makemigrations"
+echo "### list"
+python3 -m pip list
+
+echo "### makemigrations"
 python3 manage.py makemigrations
-echo "collectstatic"
+echo "### collectstatic"
 python3 manage.py collectstatic
-echo "migrate"
+echo "### migrate"
 python3 manage.py migrate
-echo "create superuser"
+echo "### create superuser"
 python3 manage.py createsuperuser --username admin --email admin@example.com --noinput
