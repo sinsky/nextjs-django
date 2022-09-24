d47f46337dd8e2195a7f7d52025ccfdcf2327858
sinsky <60662802+sinsky@users.noreply.github.com>
2022-09-24 20:18:38 +0900

    add document file

---
+++ nextjs-tailwind/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="AAeVstj1YXXxeU6GD51aC0kMRYajMXSOEd6bSdJmFHg" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
