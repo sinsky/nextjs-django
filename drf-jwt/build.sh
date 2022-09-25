76a8be71f8703f6fb343b0d0d89c99435cf6ba5b
sinsky <60662802+sinsky@users.noreply.github.com>
2022-09-25 19:04:23 +0900

    update build.sh

--- drf-jwt/build.sh
+++ drf-jwt/build.sh
@@ -6,7 +6,9 @@ pip install -r requirements.txt
 
 echo "### collectstatic"
 python manage.py collectstatic --noinput
+echo "### makemigrations"
+python manage.py makemigrations
 echo "### migrate"
 python manage.py migrate
-echo "create super user"
+# echo "create super user"
 #python manage.py createsuperuser --username admin --email example@example.com --noinput
