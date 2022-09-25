6f525f492c9d3a1cf4cc8cfb39a5898446ee85f2
sinsky <60662802+sinsky@users.noreply.github.com>
2022-09-25 18:44:30 +0900

    update build.sh, deployed is commentout createsuperuser

--- drf-jwt/build.sh
+++ drf-jwt/build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+echo "### pip install"
 pip install -r requirements.txt
 
+echo "### collectstatic"
 python manage.py collectstatic --noinput
+echo "### migrate"
 python manage.py migrate
-python manage.py createsuperuser --username admin --email example@example.com --noinput
+echo "create super user"
+#python manage.py createsuperuser --username admin --email example@example.com --noinput
